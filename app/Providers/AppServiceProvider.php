9c69a6237e28db0bcddfd0d9d4ce453f8537a7ee
Kirill <abramovku@gmail.com>
2025-05-03 12:51:09 +0300

    hh-auth fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,13 @@
 
 namespace App\Providers;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\MessageFormatter;
+use GuzzleHttp\Middleware;
 use Illuminate\Support\ServiceProvider;
+use Monolog\Handler\RotatingFileHandler;
+use Monolog\Logger;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +25,34 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->app->bind('GuzzleClient', function () {
+
+            $stack = HandlerStack::create();
+
+            if (config('services.guzzle.log') === true) {
+                $messageFormats = [
+                    'REQUEST: {method} - {uri} - HTTP/{version} - {req_headers} - {req_body}',
+                    'RESPONSE: {code} - {res_body}',
+                ];
+
+                collect($messageFormats)->each(function ($messageFormat) use ($stack) {
+                    // We'll use unshift instead of push, to add the middleware to the bottom of the stack, not the top
+                    $stack->unshift(
+                        Middleware::log(
+                            with(new Logger('guzzle-log'))->pushHandler(
+                                new RotatingFileHandler(storage_path('logs/guzzle-log.log'))
+                            ),
+                            new MessageFormatter($messageFormat)
+                        )
+                    );
+                });
+            }
+
+            return function ($config = []) use ($stack) {
+                return new Client(array_merge($config, ['handler' => $stack,
+                    'timeout' => 60, 'connect_timeout' => 60
+                ]));
+            };
+        });
     }
 }
