02732e2e8227f40c337a03942733dc0ec8490ac6
abramovku <abramovku@gmail.com>
2025-06-27 16:04:28 +0300

    twin

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->validateCsrfTokens(except: [
-            route('estaff.webhook'),
+            '/estaff-webhooks',
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
