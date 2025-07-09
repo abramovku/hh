c4ecd39caf8b625a3e54088359f04435057959fc
abramovku <abramovku@gmail.com>
2025-07-09 19:00:32 +0300

    twin

--- app/Http/Controllers/EndpointController.php
+++ app/Http/Controllers/EndpointController.php
@@ -11,34 +11,81 @@ class EndpointController extends Controller
 {
     public function create(AddCandidate $request)
     {
-        Log::channel('twin')->info("twin create candidate", [$request->all()]);
-
-        $EstaffService = app('estaff');
-        $response = $EstaffService->addResponse($request->all());
-
-        Log::channel('twin')->info("twin create candidate response", [$response]);
+        Log::channel('app')->info("twin create candidate", [$request->all()]);
+        try {
+            $EstaffService = app('estaff');
+            $response = $EstaffService->addResponse($request->all());
+        } catch (\Exception $e) {
+            Log::channel('app')->error(
+                'Estaff service error',
+                [
+                    'message' => $e->getMessage(),
+                    'file' => $e->getFile(),
+                    'line' => $e->getLine(),
+                ]
+            );
+            return response()->json([
+                'success' => false,
+                'message' => 'Estaff return error.',
+                'error' => $e->getMessage(),
+                'data' => [],
+            ]);
+        }
+        Log::channel('app')->info("twin create candidate response", [$response]);
         return response()->json($response, 200);
     }
 
     public function update(UpdateCandidate $request)
     {
-        Log::channel('twin')->info("twin change candidate", [$request->all()]);
-
-        $EstaffService = app('estaff');
-        $response = $EstaffService->changeCandidate($request->all());
-
-        Log::channel('twin')->info("twin change candidate response", [$response]);
+        Log::channel('app')->info("twin change candidate", [$request->all()]);
+        try {
+            $EstaffService = app('estaff');
+            $response = $EstaffService->changeCandidate($request->all());
+        } catch (\Exception $e) {
+            Log::channel('app')->error(
+            'Estaff service error',
+            [
+            'message' => $e->getMessage(),
+            'file' => $e->getFile(),
+            'line' => $e->getLine(),
+            ]
+            );
+            return response()->json([
+            'success' => false,
+            'message' => 'Estaff return error.',
+            'error' => $e->getMessage(),
+            'data' => [],
+            ]);
+        }
+        Log::channel('app')->info("twin change candidate response", [$response]);
         return response()->json($response, 200);
     }
 
     public function state(SetStateCandidate $request)
     {
-        Log::channel('twin')->info("twin set state candidate", [$request->all()]);
+        Log::channel('app')->info("twin set state candidate", [$request->all()]);
+        try {
+            $EstaffService = app('estaff');
+            $response = $EstaffService->setStateCandidate($request->all());
+        } catch (\Exception $e) {
+            Log::channel('app')->error(
+                'Estaff service error',
+                [
+                    'message' => $e->getMessage(),
+                    'file' => $e->getFile(),
+                    'line' => $e->getLine(),
+                ]
+            );
+            return response()->json([
+                'success' => false,
+                'message' => 'Estaff return error.',
+                'error' => $e->getMessage(),
+                'data' => [],
+            ]);
+        }
 
-        $EstaffService = app('estaff');
-        $response = $EstaffService->setStateCandidate($request->all());
 
-        Log::channel('twin')->info("twin set state candidate response", [$response]);
+        Log::channel('app')->info("twin set state candidate response", [$response]);
         return response()->json($response, 200);
     }
 }
