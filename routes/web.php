71507bbcf17de273b218e89854f40a2e15c09595
abramovku <abramovku@gmail.com>
2025-07-09 15:32:37 +0300

    twin

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\CallbackController;
 use \App\Http\Controllers\WebhookController;
+use \App\Http\Controllers\EndpointController;
 
 Route::get('/', function () {
     return app()->version();
