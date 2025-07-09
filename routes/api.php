7674e4578b5d40e1ac51f99ce578a3b4f4caa021
Kirill <abramovku@gmail.com>
2025-07-09 17:09:57 +0300

    db

---
+++ routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::post('estaff-webhooks', [WebhookController::class, 'estaffWebhooks'])->name('estaff.webhook');
+Route::post('twin-webhooks', [WebhookController::class, 'twinWebhooks'])->name('twin.webhook');
+Route::group(['as' => 'twin.', 'prefix' => 'twin'], function () {
+    Route::post('createCandidate', [EndpointController::class, 'create'])->name('twin.create');
+    Route::post('updateCandidate', [EndpointController::class, 'update'])->name('twin.update');
+    Route::post('stateCandidate', [EndpointController::class, 'state'])->name('twin.state');
+});
